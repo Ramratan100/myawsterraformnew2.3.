33305c9aa927ec9c0d0adeec5e26a5df49c35804
Ram ratan yadav <rratan100@gmail.com>
2024-12-20 09:45:11 +0530

    Delete terraform directory

--- terraform/modules/instance/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bastion_instance_id" {
-  value = aws_instance.bastion_host.id
-}
-
-output "mysql_instance_id" {
-  value = aws_instance.mysql_instance.id
-}
--- terraform/modules/security_group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
-  type        = string
-}
