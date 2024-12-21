0c14aabcd666ef0c524bbb842a8d3e20652fb587
Ram ratan yadav <rratan100@gmail.com>
2024-12-22 02:15:59 +0530

    Update main.tf

--- terraform/modules/instance/main.tf
+++ terraform/modules/instance/main.tf
@@ -1,10 +1,15 @@
+module "security_group" {
+  source = "./security_group"
+}
+
 resource "aws_instance" "bastion_host" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.public_subnet_id
-  security_groups = var.bastion_sg_id
+  ami                        = var.ami
+  instance_type              = var.instance_type
+  key_name                   = var.key_name
+  subnet_id                  = var.public_subnet_id
+  security_groups            = [module.security_group.bastion_sg_id]
   associate_public_ip_address = true
+}
 
 user_data = <<-EOF
     #!/bin/bash
@@ -203,7 +208,7 @@ resource "aws_instance" "mysql_instance" {
   instance_type = var.instance_type
   key_name      = var.key_name
   subnet_id     = var.private_subnet_id
-  security_groups = var.mysql_sg_id
+  security_groups = [module.security_group.mysql_sg_id]
 
 user_data = <<-EOF
     #!/bin/bash
