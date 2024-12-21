9b844c044f2c2c866401960eb48a6e7e68c623da
Ram ratan yadav <rratan100@gmail.com>
2024-12-22 01:13:57 +0530

    Create generate_inventory.sh

---
+++ terraform/modules/instance/generate_inventory.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+output_bastion=$(terraform output -raw bastion_instance_ip)
+output_mysql_ip=$(terraform output -raw mysql_instance_ip)
+
+if [ -z "$output_bastion" ] || [ -z "$output_mysql_ip" ]; then
+  echo "Error: Terraform outputs are missing. Ensure Terraform has been applied."
+  exit 1
+fi
+
+# Generate YAML inventory
+cat <<EOF > ../ansible/inventory.yml
+bastion:
+  hosts:
+    bastion_instance:
+      ansible_host: $output_bastion
+      ansible_user: ubuntu
+
+mysql:
+  hosts:
+    mysql_instance:
+      ansible_host: $output_mysql_ip
+      ansible_user: ubuntu
+EOF
+
+echo "Ansible inventory.yml has been successfully generated."
