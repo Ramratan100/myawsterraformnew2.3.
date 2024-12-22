dc4af6b447bcfc1f8f911e8a674a2c9d52d48221
Ram ratan yadav <rratan100@gmail.com>
2024-12-22 23:06:47 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -23,10 +23,14 @@ output "mysql_instance_id" {
   value       = module.instance.mysql_instance_id
 }
 
-output "bastion_host_id" {
-  value = module.instance.bastion_host_id
+output "bastion_host_public_ip" {
+  description = "The public IP address of the bastion host."
+  value       = module.my_module.bastion_host_public_ip
 }
 
-output "mysql_instance_id" {
-  value = module.instance.mysql_instance_id
+output "mysql_instance_private_ip" {
+  description = "The private IP address of the MySQL instance."
+  value       = module.my_module.mysql_instance_private_ip
 }
+
+
