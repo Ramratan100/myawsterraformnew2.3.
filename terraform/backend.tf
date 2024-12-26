ab81c906764704e2ed5ca5a36681cd4de2a0ba12
Ram ratan yadav <rratan100@gmail.com>
2024-12-26 15:40:44 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "kriday-bucket"        # Your S3 bucket name
     key    = "terraform/statefile.tfstate"  # The key path for the state file in the bucket
-    region = "us-east-1"              # The region where the S3 bucket is located
+    region = "ap-northeast-1"              # The region where the S3 bucket is located
   }
 }
